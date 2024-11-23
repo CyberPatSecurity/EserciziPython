c2572c47a2a049979413b7ee41cf07f3087dabfe
CyberPatSecurity <patrizioegidi@gmail.com>
2024-11-23 10:41:08 +0100

    Aggiunto programma tupla minimo e massimo valore

---
+++ Tupla_min_max.py
@@ -0,0 +1,6 @@
+#Creare una tupla di numeri interi e stampare il valore più grande e quello più piccolo
+
+tupla_ex = (1,56,67,4,89,54,89,54,78,8,3,75,45,65)
+
+print(f"il valore minimo è il seguente : {min(tupla_ex)}")
+print(f"il valore massimo è il seguente : {max(tupla_ex)}")
