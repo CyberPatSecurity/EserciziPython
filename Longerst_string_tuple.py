bb429162a6aa5427289dd5ca927ba3a229bd5b78
CyberPatSecurity <patrizioegidi@gmail.com>
2024-11-26 09:34:56 +0100

    Stringa più lunga in una tupla

--- Longerst_string_tuple.py
+++ Longerst_string_tuple.py
@@ -1,6 +1,14 @@
-#
 #Creare una tupla di stringhe e stampare la stringa più lunga.
 
-tupla1 = ('pappa','cacca','nanna','limone')
-long_index = len(tupla1[3])
-print(long_index)
+tupla1 = ('pappa','roma','nanna','limone')
+
+lenght_string = []
+
+for stringa in tupla1:
+    long_index = len(stringa)
+    lenght_string.append(long_index)
+
+valore_massimo = max(lenght_string)
+indice_max = lenght_string.index(valore_massimo)
+
+print(tupla1[indice_max])
