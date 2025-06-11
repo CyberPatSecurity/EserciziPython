0f69b5114234a19aa0b4b3ec2b9a1b0346b5e658
CyberPatSecurity <130449799+CyberPatSecurity@users.noreply.github.com>
2025-06-11 06:53:32 +0200

    Update scuola.py

--- scuola.py
+++ scuola.py
@@ -25,7 +25,7 @@ class Studente(Persona):
         
     
     def informazioni(self):
-        print(f"Nome: {self.nome}, Età: {self.età}, Media Voti: {self.media}")
+        print(f"\nNome studente: {self.nome}, Età: {self.età}, Media Voti: {self.media}\n")
         
     
 class Professore(Persona):
@@ -33,25 +33,34 @@ class Professore(Persona):
         super().__init__(nome, età)
         self.materia = materia
         
-
-
-
-    def assegna_voto(self, studente, voto):
+        
+    
+    def assegna_voto(self, voto):
         self.voto = voto
-        self.studente = Studente( nome, età, voti)
-        self.voti = Studente.voti()    
-        print(f"{self.voti}")
-
-
-    #def informazioni(self):
-    #    print(f"Nome: {self.nome}, Età: {self.età} {self.materia} {self.voto}")
+        self.studente = persona
+        self.nuovo_voto = self.studente.voti
+        self.nuovo_voto.append(voto)
+        #print(self.nuovo_voto)             
+        self.media = self.studente.media_voti()
         
 
 
+    def informazioni(self):
+        print(f"Professora Maria Teresa De Angelis - Materia: {self.materia} -> Nome Studente: {self.nome}, Età: {self.età}, Voto Matematica: {self.voto}\n")
+        print(f"\tNuova media complessiva dei voti : {self.media}\n")
+      
 
-studente = Studente("Lorenzo", 0, [7, 10, 5, 10])
-studente.media_voti()
-studente.informazioni()
-""""""
-voto_prof = Professore("Lorenzo", 0, "Matematica")
-voto_prof.assegna_voto(studente, 9)
+"""_______________________________________________________________________________________________________________________________________"""   
+
+persona = Studente("Lorenzo Egidi Fancelli", 0, [7, 10, 5, 10])
+persona.media_voti()
+persona.informazioni()
+
+"""_______________________________________________________________________________________________________________________________________"""
+
+voto_prof = Professore("Lorenzo Egidi Fancelli",0, "Matematica")
+voto_prof.assegna_voto(10)
+voto_prof.informazioni()
+
+
+
