44c9c14c5a042afecfd453ec9b3df80792b809a4
CyberPatSecurity <130449799+CyberPatSecurity@users.noreply.github.com>
2025-06-30 08:17:39 +0200

    Update Biblioteca.py

--- Biblioteca.py
+++ Biblioteca.py
@@ -7,8 +7,7 @@ class Libro:
 
 
 class Biblioteca:
-    def __init__(self, nomeBiblioteca):
-        self.nomeBiblioteca = nomeBiblioteca
+    def __init__(self):
         self.libri = []
 
     
@@ -23,44 +22,46 @@ class Biblioteca:
     def ottieni_libri_genere(self, genere):
         generi = []        
         for book in self.libri:
-            if book.genere == genere:
+            if book.genere.lower == genere.lower:
                 generi.append(book)
         return generi
     
 
-    def ottieni_libri_usciti_prima_di(self):
+    def ottieni_libri_usciti_prima_di(self, anno):
         annoDiPub = []
         for book in self.libri:
-            if book.anno < 2000:
+            if book.anno < anno:
                 annoDiPub.append(book)
         return annoDiPub
 
 
-libro1 = Libro("Respiro", "Ted Chiang", 2015, "Fantascienza")
-libro2 = Libro("Anna Karenina", "Lev TOlstoj", 1840, "Storico")
-libro3 = Libro("It", "Stephen King", 1990, "Horror")
-libro4 = Libro("Neuromante", "William Gibson", 1980, "Fantascienza")
-libro5 = Libro("Tutti i racconti", "Franz Kafka", 1920, "Grottesco")
+l1 = Libro("Respiro", "Ted Chiang", 2015, "Fantascienza")
+l2 = Libro("Anna Karenina", "Lev TOlstoj", 1840, "Storico")
+l3 = Libro("It", "Stephen King", 1990, "Horror")
+l4 = Libro("Neuromante", "William Gibson", 1980, "Fantascienza")
+l5 = Libro("Tutti i racconti", "Franz Kafka", 1920, "Grottesco")
 
-mia_biblioteca = Biblioteca("Biblioteca Nazionale")
+biblioteca = Biblioteca()
 
-mia_biblioteca.aggiungi_libro(libro1)
-mia_biblioteca.aggiungi_libro(libro2)
-mia_biblioteca.aggiungi_libro(libro3)
-mia_biblioteca.aggiungi_libro(libro4)
-mia_biblioteca.aggiungi_libro(libro5)
+biblioteca.aggiungi_libro(l1)
+biblioteca.aggiungi_libro(l2)
+biblioteca.aggiungi_libro(l3)
+biblioteca.aggiungi_libro(l4)
+biblioteca.aggiungi_libro(l5)
 
-mia_biblioteca.ottieni_libri_genere("Horror")
+biblioteca.ottieni_libri_genere("Horror")
 
 
+# 1. Tutti i libri archiviati
+print("Catalogo completo:")
+for libro in biblioteca.libri_archiviati():
+    print("  ", libro.titolo)
 
-for book in mia_biblioteca.libri:
-    print(f"Libro -> Titolo - {book.titolo}, Autore - {book.autore}, Anno di pubblicazione - {book.anno}, Genere - {book.genere}\n")
+#Libri di genere Fantascientifico
+print("\nLibri di fantascienza: ")
+for libro in biblioteca.ottieni_libri_genere("Fantascienza"):
+    print(" ", libro.titolo)
 
-genere_libro = mia_biblioteca.ottieni_libri_genere("Horror")
-for book in genere_libro:
-    print(f"Lista dei Libri di {book.genere} : {book.titolo}")
-
-anno = mia_biblioteca.ottieni_libri_usciti_prima_di()
-for book in anno:
-    print(f"Lista dei Libri usciti prima dell'anno 2000 : {book.titolo}")
+print("\nLibri usciti prima del 2000")
+for libro in biblioteca.ottieni_libri_usciti_prima_di(2000):
+    print(" ", libro.titolo)
